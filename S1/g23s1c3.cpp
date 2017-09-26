9ba1ca33e49f6a3bd0f0ade8c13c65aa28b37f3a
Milton Freitas <miltonfreits@hotmail.com>
2017-09-26 23:24:45 +0100

    donezo

--- S1/g23s1c3.cpp
+++ S1/g23s1c3.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-  int n_max = 10;
+  int n_max = 0;
   int n = 0;
   float r = 0, x = 0; // r eh o parametro de controle
 
@@ -15,21 +15,17 @@ int main()
   ifstream dados;
   dados.open("dados.txt");
   dados >> r >> x >> n_max;
-  cout << r << endl;
-  cout << x << endl;
-  cout << n_max << endl;
+  cout << r << endl << x << endl << n_max << endl;
   dados.close();
   ofstream dados2;
   dados2.open("dados2.txt");
-   for (n = 1; n <= n_max; n++)
-   {
-     dados2 << x << " ";
-     x = x*r*(1.-x);
-    //cout << "x(" << n << ")= " << x << endl;
-    dados2 << x << endl;
-   }
-   dados2.close();
+    for (n = 1; n <= n_max; n++)
+    {
+      x = x*r*(1.-x);
+      //cout << "x(" << n << ")= " << x << endl;
+      dados2 << x << endl;
+    }
+
+  dados2.close();
   return 0;
 }
-//memes
-// a lot of memes
