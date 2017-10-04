5c18564c924cec56f4895d6e17f63c161a3f240b
Miguel Paradinha <miguel.p.paradinha@gmail.com>
2017-10-04 12:04:19 +0100

    metropolis main file

--- S2/g23s2c4.cpp
+++ S2/g23s2c4.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include <cmath>
+
+#include "markov.h"
+
 using namespace std;
-srand()
+
+// probability function
+float p(float x) {}
+// function to integrate
+float f(float x) {}
 
 int main ()
 {
+    int n, m;
+    // read arguments from user
+    cout << "Markov chain size: "; cin >> n;
+    cout << "m: "; cin >> m;
+    float* markov_chain = markov(n, m, &p); 
 
+    // calculate integral
+    float integral = 0;
+    for(int i = 0; i < n; i++) integral += f(markov_chain[i]);
+    integral /= n;
 
-
-
+    // print to cout and file the result
+    cout << "Integral value: " << integral << endl;
 }
