034045bf2dc7021183515747fd13dd030a75fa24
Milton Freitas <miltonfreits@hotmail.com>
2017-11-18 22:41:58 +0000

    gh

--- g23p3/matrix.h
+++ g23p3/matrix.h
@@ -8,24 +8,104 @@
 class Matrix {
     double **p;
 
+
+
 public:
-    Matrix(int n1,int n2 ){
+    int _nx, _ny;
+    Matrix(int n1,int n2 )
+    {
+        nx = n1;
+        ny = n2;
         double **v = new double*[n1];
-        for(int = 0; i < n1; i++)
+        for(int i = 0; i < n1; i++)
         {
             p[i] = new double[n2];
         }
     }
 
-    
+    ~Matrix()
+    {
+        for(int i = 0; i <n1; i++ )
+        {
+            delete[] p[i];
+        }
+        delete[] p;
+    }
+    Matrix& operator=(const Matrix& r) {
+        assert(nx == r.nx && ny == r.ny);
+         // so podemos copiar vetores do mesmos tamanho
+        for(int i = 0; i < nx; i++)
+        {
+            for(int j = 0; j < ny;j++ )
+            {
+                this->p.get(i,j) = r.get(i,j);
+            }
+        }
+    }
+    double& get(int x, int y) {return p[x][y];}
+
+    Matrix& operator+=(const Matrix& r) {
+        assert(nx == r.nx && ny == r.ny); // so podemos adicionar matrizes do mesmos tamanho
+        for(int i = 0; i < nx; i++)
+        {
+            for(int j = 0; j < ny;j++ )
+            {
+                this->p.get(i,j) += r.get(i,j);
+            }
+        }
+    }
+
+
+
 
 };
 
+Matrix operator*(double a, const Matrix& r)
+{
+    Vector result(r.nx,r.ny);
+    for(int i = 0; i < r.nx; i++)
+    {
+        for(int j = 0; j < r.ny; j++)
+        {
+            result.get(i,j) = r.get(i,j) * a;
+        }
 
 
+    }
 
 
+    return result;
+}
 
+Matrix operator+(const Matrix& l, const Matrix& r)
+{
+    Vector result(r.nx,r.ny);
+    for(int i = 0; i < r.nx; i++)
+    {
+        for(int j = 0; j < r.ny; j++)
+        {
+            result.get(i,j) = r.get(i,j) + l.get(i,j);
+        }
+    }
+
+
+    return result;
+}
+
+Matrix operator-(const Matrix& l, const Matrix& r)
+{
+    Vector result(r.nx,r.ny);
+    for(int i = 0; i < r.nx; i++)
+    {
+        for(int j = 0; j < r.ny; j++)
+        {
+            result.get(i,j) = l.get(i,j) - r.get(i,j);
+        }
+    }
+
+
+    return result;
+}
 
 
 
